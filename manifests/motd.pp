8f5dbef7a5048fe5a9a66619141dd23ffac6dd3a
puppetassignment <PuppetAssigment@Xoriant.Com>
2015-07-02 17:27:25 +0530

    updated content

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -4,7 +4,7 @@
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "hello, World! Welcome to PE !!\n",
+    content => "hello, World! Welcome to Puppet Enterprise !!\n",
     }
 
  }
